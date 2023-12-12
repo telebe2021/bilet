91cbe1db4b7cf62a5859d092138a0a7ba7ae15ca
telebe2021 <mahsungulmmmdov@gmail.com>
2023-12-12 14:58:39 +0400

    commit olundu

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
-//////////NAVBAR//////////
 
-window.addEventListener("scroll", ()=>{
-    if(window.scrollY > 100){
-        document.querySelector("nav").style.backgroundColor = "rgba(0, 0, 0, 0.8)"
+
+
+window.addEventListener("scroll",()=>{
+    if(window.scrollY>50){
+        document.querySelector("nav").style.backgroundColor="rgba(0,0,0,0.7)"
     }
     else{
-        document.querySelector("nav").style.backgroundColor = "rgba(0, 0, 0, 0)"
-    }
-});
-
-//////////MENU//////////
-
-const menu = document.querySelector(".menu-div");
-function hideMenu() {
-    document.querySelector(".menu-bg").style.display = "none";
-}
-
-document.querySelector("#click").addEventListener("click", ()=>{
-    event.preventDefault();
-    document.querySelector(".menu-bg").style.display = "block";
-});
-
-document.querySelector(".close").addEventListener("click", hideMenu());
-
-document.addEventListener("click", (e)=>{
-    if(!menu.contains(e.target) && e.target !== document.querySelector("#click")){
-        hideMenu();
+        document.querySelector("nav").style.backgroundColor="rgba(0,0,0,0)"
     }
 })
 
-window.addEventListener('resize', () => {
-    if (window.innerWidth > 768) {
-        hideMenu();
-    }});
+fetch("http://localhost:3000/card")
+.then(res =>res.json())
+.then(data => {
+        console.log(data);
+        data.forEach(card => {
+            document.querySelector(".sec11").innerHTML+=`<div>
+                    
+            <img src="${card.image}" alt="">
+            <a href="#"><h4>${card.link}</h4></a>
+            <p>${card.text}</p>
+            <span><button><i class="bi bi-trash"></i> delete</button>
+            <button><i class="bi bi-arrow-repeat"></i> update</button>
+            </span>
+        </div>`
 
-//////////DATA//////////
+    });
+})
