a6e51ce2ab926c6b6e407c20aa850e23515ad053
telebe2021 <mahsungulmmmdov@gmail.com>
2023-12-14 17:05:57 +0400

    commit olundu

--- assets/Scripts/script.js
+++ assets/Scripts/script.js
@@ -16,6 +16,7 @@ fetch("http://localhost:3000/card")
         console.log(data);
         data.forEach(card => {
             document.querySelector(".uclu").innerHTML+=`<div class="sec11">
+        
             <i class="bi bi-heart"></i>
             <img src="${card.image}" alt="">
             <a href="#"><h4>${card.link}</h4></a>
@@ -32,6 +33,48 @@ fetch("http://localhost:3000/card")
     });
 })
 
+function createCardElement(card) {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card");
+    cardElement.dataset.id = card.id;
+    cardElement.innerHTML = `
+      <img src="${card.image}" alt="Card Image">
+      <h3>${card.link}</h3>
+    `;
+    cardElement.addEventListener("click", () => showCardInfo(card));
+    return cardElement;
+  }
+  
+  // Her bir kartın bilgilerini göster
+  function showCardInfo(card) {
+    const infoContainer = document.getElementById("infoContainer");
+    infoContainer.innerHTML = `
+      <h2>ID: ${card.id}</h2>
+      <img src="${card.image}" alt="Card Image">
+      <h3>${card.link}</h3>
+      <p>${card.text}</p>
+    `;
+  }
+  
+  // Arama alanındaki değeri dinle ve tabloyu filtrele
+  const searchInput = document.getElementById("searchInput");
+  searchInput.addEventListener("input", () => {
+    const searchText = searchInput.value.toLowerCase();
+  
+    // Filtreleme işlemi
+    const filteredCards = jsonData.card.filter(card => {
+      return card.link.toLowerCase().includes(searchText);
+    });
+  
+    // Filtrelenmiş kartları göster
+    const searchResultsContainer = document.getElementById("searchResultsContainer");
+    searchResultsContainer.innerHTML = "";
+    filteredCards.forEach(filteredCard => {
+      const filteredCardElement = createCardElement(filteredCard);
+      searchResultsContainer.appendChild(filteredCardElement);
+    });
+  });
+  
 function addEventListener(id) {
     axios.get("http://localhost:3000/boxs/"+id)
     .then(res=>{
